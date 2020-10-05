72f105630ee4fc480b84452324bd4ef12c0c242c
endritsul <62520184+endritsul@users.noreply.github.com>
2020-10-05 20:32:15 +0200

    Add files via upload

---
+++ fakultaet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakultätVonN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int zahl;
+            int result = 1;
+            string streingabe;
+
+            Console.WriteLine("Zahl eingeben");
+            streingabe = Console.ReadLine();
+            zahl = Convert.ToInt32(streingabe);
+
+            for (int i = 1; i<=zahl; i++)
+            {
+                result = result * i;
+            }
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
